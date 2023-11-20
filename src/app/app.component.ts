0a9c3761df7551e9153889b3f843c3a13a26c50d
Paul Greetham <pgreetham@protonmail.com>
2023-11-20 09:31:53 +0100

    news section added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./Components/navbar/navbar.component";
 import { HomeComponent } from "./Pages/home/home.component";
+import { NewsComponent } from './Pages/news/news.component';
 import { ExperienceComponent } from "./Pages/experience/experience.component";
 import { EducationComponent } from "./Pages/education/education.component";
 import { PublicationsComponent } from "./Pages/publications/publications.component";
@@ -13,7 +14,7 @@ import { ContactComponent } from "./Pages/contact/contact.component";
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    imports: [CommonModule, RouterOutlet, NavbarComponent, HomeComponent, ExperienceComponent, EducationComponent, PublicationsComponent, ContactComponent]
+    imports: [CommonModule, RouterOutlet, NavbarComponent, HomeComponent, NewsComponent, ExperienceComponent, EducationComponent, PublicationsComponent, ContactComponent]
 })
 export class AppComponent {
   title = 'dagmarheinrich';
